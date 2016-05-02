b47d6494a7bc02396f8d8cf94c10b2f0bfce833c
jackchua <jackcaihe@outlook.com>
2016-05-02 12:57:39 +0800

    更新了状态栏的背景颜色
    更新了状态栏的背景颜色

--- Durian/app/src/main/java/com/durian/sixkids/durian/MusicMainActivity.java
+++ Durian/app/src/main/java/com/durian/sixkids/durian/MusicMainActivity.java
@@ -1,13 +1,34 @@
 package com.durian.sixkids.durian;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.readystatesoftware.systembartint.SystemBarTintManager;
 
 public class MusicMainActivity extends AppCompatActivity {
-
+    private SystemBarTintManager tintManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+       this.supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+       // initWindow();
         setContentView(R.layout.activity_music_main);
+       getWindow().setFlags(Window.FEATURE_NO_TITLE, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+
+    private void initWindow(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            tintManager = new SystemBarTintManager(this);
+            tintManager.setStatusBarTintColor(Color.parseColor("#c1c1be"));
+            tintManager.setStatusBarTintEnabled(true);
+        }
     }
 }
