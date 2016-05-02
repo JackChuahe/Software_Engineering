0cda2ea31b0eddf002a40408bd383971a5c5b416
jackchua <jackcaihe@outlook.com>
2016-05-02 09:26:42 +0800

    搭建好项目的框架
    搭建好项目的框架，开发环境 android studio 2.1 beta 5
    SDK 5.0

---
+++ Durian/app/src/main/java/com/durian/sixkids/durian/MusicMainActivity.java
@@ -0,0 +1,13 @@
+package com.durian.sixkids.durian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MusicMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music_main);
+    }
+}
