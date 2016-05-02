ae9c1d02bbb399b0990a7b892c19595a2c8ef3da
wostilling <sccxyc@sina.com>
2016-05-02 18:18:53 +0800

    本地音乐管理页面v1.0

--- Durian/app/src/main/java/com/durian/sixkids/durian/me/LocalManagement.java
+++ Durian/app/src/main/java/com/durian/sixkids/durian/me/LocalManagement.java
@@ -1,7 +1,31 @@
 package com.durian.sixkids.durian.me;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.durian.sixkids.durian.R;
+
 /**
  * Created by JackCai on 2016/5/2.
  */
-public class LocalManagement {
+public class LocalManagement extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.localmanagement_fragment);
+    }
+
+
+    //    @Nullable
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+//
+//
+//        return super.onCreateView(inflater, container, savedInstanceState);
+//    }
 }
